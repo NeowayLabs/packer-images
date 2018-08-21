47256c22612fef543694d23a56d712c8fc1fb8a0
evaldofelipe <contato@evaldofelipe.com>
2018-08-21 17:51:13 -0300

    Fix and run environment for tester

--- terraform/providers/azure/images-tester/tester-environment.tf
+++ terraform/providers/azure/images-tester/tester-environment.tf
@@ -1,5 +1,6 @@
 
 # Set provider and terraform version
+
 provider "azurerm" {
   version = "~> 1.1"
 }
@@ -8,130 +9,100 @@ terraform {
   required_version = "0.11.7"
 }
 
-# Create resource group
-resource "azurerm_resource_group" "builder_rg" {
-  name     = "${var.builder_resource_group_name}"
+#Create resource group
+
+resource "azurerm_resource_group" "tester_rg" {
+  name     = "${var.prefix}-resource-group"
   location = "${var.location}"
 }
 
 # Create Vnet
-resource "azurerm_virtual_network" "builder_vnet" {
-    name                 = "${var.builder_resource_group_name}-vnet"
-    address_space        = "${var.builder_vnet}"
+
+resource "azurerm_virtual_network" "tester_vnet" {
+    name                 = "${var.prefix}-vnet"
+    address_space        = ["${var.tester_vnet}"]
     location             = "${var.location}"
-    resource_group_name  = "${var.builder_resource_group_name}"
+    resource_group_name  = "${var.tester_resource_group_name}"
 }
 
 # Create Subnet
-resource "azurerm_subnet" "builder_subnet" {
-    name                 = "${var.builder_env}-subnet"
-    resource_group_name  = "${var.builder_resource_group_name}"
-    virtual_network_name = "${azurerm_virtual_network.builder_vnet.name}"
-    address_prefix       = "${var.builder_subnet}"
+
+resource "azurerm_subnet" "tester_subnet" {
+    name                 = "${var.prefix}-subnet"
+    resource_group_name  = "${var.tester_resource_group_name}"
+    virtual_network_name = "${azurerm_virtual_network.tester_vnet.name}"
+    address_prefix       = "${var.tester_subnet}"
 }
 
 # Provisioning public IP
-resource "azurerm_public_ip" "builder_public_ip" {
-    name                         = "${var.builder_env}-public-ip"
+
+resource "azurerm_public_ip" "tester_public_ip" {
+    name                         = "${var.prefix}-public-ip"
     location                     = "${var.location}"
-    resource_group_name          = "${var.builder_resource_group_name}"
+    resource_group_name          = "${var.tester_resource_group_name}"
     public_ip_address_allocation = "dynamic"
 }
 
-# # Provisioning eth (w/ public IP)
-resource "azurerm_network_interface" "builder_nic" {
-    name                      = "${var.builder_nic_name}"
+# Provisioning eth (w/ public IP)
+
+resource "azurerm_network_interface" "tester_nic" {
+    name                      = "${var.tester_nic_name}"
     location                  = "${var.location}"
-    resource_group_name       = "${var.builder_resource_group_name}"
-    network_security_group_id = "${azurerm_network_security_group.builder_nsg.id}"
+    resource_group_name       = "${var.tester_resource_group_name}"
 
     ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = "${azurerm_subnet.builder_subnet.id}"
+        name                          = "nic-config"
+        subnet_id                     = "${azurerm_subnet.tester_subnet.id}"
         private_ip_address_allocation = "dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.builder_public_ip.id}"
+        public_ip_address_id          = "${azurerm_public_ip.tester_public_ip.id}"
     }
 
 }
 
-# Create Network Security Group and rule for SSH access
-resource "azurerm_network_security_group" "builder_nsg" {
-    name                = "${var.builder_env}-nsg"
-    location            = "${var.location}"
-    resource_group_name = "${var.builder_resource_group_name}"
+# Set image to terraform use on VM
+# This Custom Image already builded by packer
 
-    security_rule {
-        name                       = "Allow ssh"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+data "azurerm_image" "tester_image" {
+  name                = "${var.builder_image_name}"
+  resource_group_name = "${var.builder_resource_group_name}"
 }
 
-# # Image from packer reference to build a new VM
-# data "azurerm_image" "builder_image" {
-#   name                = "${var.packer_image_name}"
-#   resource_group_name = "${var.packer_resource_group_name}"
-# }
+# Create virutal machine
 
+resource "azurerm_virtual_machine" "tester_vm" {
+  name                  = "${var.prefix}-vm"
+  location              = "${var.location}"
+  resource_group_name   = "${var.tester_resource_group_name}"
+  network_interface_ids = ["${azurerm_network_interface.tester_nic.id}"]
+  vm_size               = "${var.tester_vm_size}"
 
-resource "azurerm_image" "builder_image" {
-  name     = "${var.packer_image_name}"
-  location = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.builder_rg.name}"
-  os_disk {
-     os_type ="linux"
-     os_state = "Generalized"
-     caching = "ReadWrite"
-  }
-}
+  # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This is used here only for a tester environment
 
+  delete_os_disk_on_termination = true
 
-
-
-# Create virtual machine
-resource "azurerm_virtual_machine_scale_set" "builder-vm" {
-  name                = "${var.builder_vm_name}"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.builder_rg.name}"
-
-  delete_os_disk_on_termination    = "true"
-  network_interface_ids            = ["${azurerm_network_interface.builder_nic.id}"]
-  vm_size                          = "${var.builder_vm_size}"
-
-  sku {
-    name     = "Standard_DS1_v2"
-    tier     = "Standard"
-    capacity = 2
+  storage_image_reference {
+    id = "${data.azurerm_image.tester_image.id}"
   }
 
-  storage_profile_image_reference {
-    id="${azurerm_image.builder_image.id}"
-  }
-
-  storage_profile_os_disk {
-    name              = ""
+  storage_os_disk {
+    name              = "osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    admin_username = "${var.builder_user}"
+    admin_username = "${var.tester_user}"
     admin_password = ""
-    computer_name  = "${var.builder_vm_name}"
+    computer_name  = "${var.tester_vm_name}"
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
 
     ssh_keys {
-      path     = "${var.builder_user}/.ssh/authorized_keys"
+      path     = "/home/${var.tester_user}/.ssh/authorized_keys"
       key_data = "${file("~/.ssh/id_rsa.pub")}"
     }
   }
