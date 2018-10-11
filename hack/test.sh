cdf3d1c185968c98d997e15685ebdf155217210a
evaldofelipe <contato@evaldofelipe.com>
2018-10-11 10:28:49 -0300

    Add sudo to generate keys

--- hack/test.sh
+++ hack/test.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 
 #Generate a temporary key
-ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+sudo ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
 
 #Initialize packer environment
 make terraform-init env=images-builder
