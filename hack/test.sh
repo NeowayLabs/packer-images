40ef5991f0d3a829c3bdedf10e94c840285bd8fb
evaldofelipe <contato@evaldofelipe.com>
2018-10-10 22:37:08 -0300

    Add tests for travis

---
+++ hack/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+make setup
+
+#Initialize packer environment
+make terraform-init env=images-builder
+make terraform-apply env=images-builder
+
+make packer-build
+
+#Initialize tester environment
+make terraform-init env=images-tester
+make terraform-apply env=images-tester
+
+#todo test goss
+
+#Destroy tester environment
+make terraform-destroy env=images-tester
