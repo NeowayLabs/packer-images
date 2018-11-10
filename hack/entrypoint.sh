f56d6b321beb4c6a48755285e2cfd0a6c6dec6e9
evaldofelipe <contato@evaldofelipe.com>
2018-11-09 23:19:06 -0200

    Add entrypoint hack to container

---
+++ hack/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+
+declare _path_="$(pwd)"
+declare status="0"
+
+echo ${_path_} | grep google-cloud > /dev/null 2>&1 || status="1"
+
+if [[ ${status} = "0" ]]; then
+	if [[ -z ${TF_VAR_gcp_token} ]]; then
+		echo "Error: You need declare and export TF_VAR_gcp_token variable"
+		exit 1
+	fi
+	echo ${TF_VAR_gcp_token} > /tmp/google-cloud-account.json
+fi
+
+/bin/bash -c "${*}"
