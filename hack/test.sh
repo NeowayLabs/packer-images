804359163fed2599dfd37d494c18b2734fa1854c
evaldofelipe <contato@evaldofelipe.com>
2018-11-09 23:13:31 -0200

    Add gcp to travis

--- hack/test.sh
+++ hack/test.sh
@@ -3,6 +3,23 @@
 set -o errexit
 set -o nounset
 
+### Google Tests ###
+
+#Execute packer
+make packer-build env=google-cloud image=image-ubuntu
+
+#Initialize tester environment
+make terraform-init provider=google-cloud env=images-tester
+make terraform-apply provider=google-cloud env=images-tester
+
+#todo test goss
+
+#Destroy tester environment
+make terraform-destroy provider=google-cloud env=images-tester
+
+
+### Azure Tests ###
+
 #Initialize packer environment
 make terraform-init provider=azure env=images-builder
 make terraform-apply provider=azure env=images-builder
