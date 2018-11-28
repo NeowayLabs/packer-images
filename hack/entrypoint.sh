c7a92bc5d1494d934ea7e041448b212262d060d8
evaldofelipe <contato@evaldofelipe.com>
2018-11-28 16:35:22 -0200

    Adjust travis variable

--- hack/entrypoint.sh
+++ hack/entrypoint.sh
@@ -8,7 +8,7 @@ declare status="0"
 echo ${_path_} | grep google-cloud > /dev/null 2>&1 || status="1"
 
 if [[ ${status} = "0" ]]; then
-	if [[ -z ${TF_VAR_gcp_token} ]]; then
+	if [[ -z ${TRAVIS_gcp_token} ]]; then
 		echo "Error: You need declare and export TF_VAR_gcp_token variable"
 		exit 1
 	fi
